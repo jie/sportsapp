6ac5dcc05701cb901b05dab7b13a9b63b1c30210
ZhouYang <zhouyang.me@gmail.com>
2014-09-04 20:17:43 +0800

    fix shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pkill -f 'gunicorn -w 4 -b 127.0.0.1:5001 --log-config /usr/webapps/python/sportsapp/logging.conf app:app'
+pkill -f '/usr/bin/python /usr/local/bin/gunicorn -w 4 -b 127.0.0.1:5001 --log-config /usr/webapps/python/sportsapp/logging.conf -D app:app'
